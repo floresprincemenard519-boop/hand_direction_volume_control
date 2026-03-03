3ff69198213e7f6062def0ad2d73a897e959634b
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-03 22:26:48 +0800

    made a loop where each frame can be processed one at a time.

--- hand_direction_volume_control.py
+++ hand_direction_volume_control.py
@@ -7,6 +7,11 @@ capture = cv2.VideoCapture(0)
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(static_image_mode=False, max_num_hands=1, min_detection_confidence=0.5)
 
-# create a frame or the detected hand so that you can see when the mediapipe is detecting my hand.
+# create a frame for the detected hand so that you can see when the mediapipe is detecting my hand.
 mp_drawing = mp.solutions.drawing_utils
 
+# a loop where each frame can be processed and edited. this needed by mediapipe.
+while True:
+    ret, frame = capture.read()
+    if not ret:
+        break
