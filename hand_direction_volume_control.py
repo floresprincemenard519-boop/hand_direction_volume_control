37c796a6b5e81aa4f28b372cbe081c22bc75746f
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-03 22:16:37 +0800

    Initialized the mediapipe hand detectiona and the camera cature of the computer.

--- hand_direction_volume_control.py
+++ hand_direction_volume_control.py
@@ -2,3 +2,7 @@ import cv2
 import mediapipe as mp
 import pyautogui
 
+# initialize video capture and hand detection of mediapipe
+capture = cv2.VideoCapture(0)
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(static_image_mode=False, max_num_hands=1, min_detection_confidence=0.5)
