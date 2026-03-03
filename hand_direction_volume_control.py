c3db8f560e829444756e03cd729b9c05625e9fee
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-03 22:20:10 +0800

    Make a frae for my hand so that it takes shape.

--- hand_direction_volume_control.py
+++ hand_direction_volume_control.py
@@ -6,3 +6,7 @@ import pyautogui
 capture = cv2.VideoCapture(0)
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(static_image_mode=False, max_num_hands=1, min_detection_confidence=0.5)
+
+# create a frame or the detected hand so that you can see when the mediapipe is detecting my hand.
+mp_drawing = mp.solutions.drawing_utils
+
